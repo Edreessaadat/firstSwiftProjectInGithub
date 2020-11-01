352923cbd52aa2c8dd1b08ac746945a5d336e399
Mohammad Edrees <edrees_saadat@yahoo.com>
2020-10-31 23:38:29 -0400

    Initial Commit

---
+++ firstSwiftProjectInGithub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstSwiftProjectInGithub
+//
+//  Created by Mohammad Edrees on 10/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
